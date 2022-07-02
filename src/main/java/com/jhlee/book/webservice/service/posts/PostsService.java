fc64045f9e16a870859451d2c7303594ddc5ed90
qwe5507 <apvmffkdls@gmail.com>
2022-07-02 20:01:07 +0900

    feat : 수정 삭제 기능 추가

--- src/main/java/com/jhlee/book/webservice/service/posts/PostsService.java
+++ src/main/java/com/jhlee/book/webservice/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.jhlee.book.webservice.service.posts;
 
 import com.jhlee.book.webservice.web.domain.posts.Posts;
 import com.jhlee.book.webservice.web.domain.posts.PostsRepository;
+import com.jhlee.book.webservice.web.dto.PostsListResponseDto;
 import com.jhlee.book.webservice.web.dto.PostsResponseDto;
 import com.jhlee.book.webservice.web.dto.PostsSaveRequestDto;
 import com.jhlee.book.webservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,4 +37,20 @@ public class PostsService {
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다, id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
 }
