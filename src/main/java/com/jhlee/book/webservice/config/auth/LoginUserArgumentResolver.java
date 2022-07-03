fa63b2d3587c6b8ea1967a9e242dfecafd60e345
qwe5507 <apvmffkdls@gmail.com>
2022-07-03 20:52:40 +0900

    feat : Argument Resolver 사용하여 Login Session 가져오기

---
+++ src/main/java/com/jhlee/book/webservice/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,34 @@
+package com.jhlee.book.webservice.config.auth;
+
+import com.jhlee.book.webservice.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final HttpSession httpSession;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        //ArgumentResolver를 사용할지 말지 판단
+        //LoginUser 애노테이션 이면서 파라미터 클래스 타입이 SessionUser.class인 경우 True반환
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+        boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());
+
+        return isLoginUserAnnotation && isUserClass;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        return httpSession.getAttribute("user");
+    }
+}
--- src/main/java/com/jhlee/book/webservice/web/IndexController.java
+++ src/main/java/com/jhlee/book/webservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jhlee.book.webservice.web;
 
+import com.jhlee.book.webservice.config.auth.LoginUser;
 import com.jhlee.book.webservice.config.auth.dto.SessionUser;
 import com.jhlee.book.webservice.service.posts.PostsService;
 import com.jhlee.book.webservice.dto.PostsResponseDto;
@@ -19,10 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser)httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
